7a66bcb364cc5633f07b91f37312a7a58f858a42
Jon <jtwilson@ashevillenc.gov>
2022-06-28 12:58:02 -0400

    new changes

---
+++ src/test.js
@@ -0,0 +1,5 @@
+let an_array = [1,2,3,"word"]
+for (value of an_array) {
+    console.log(value)
+}
+
---
+++ src/two.js
@@ -0,0 +1,20 @@
+let myarray = [1,2,3,4,"a string"]
+
+// let mysecondarray = [myarray,myarray]
+
+// for( value of mysecondarray) 
+// {
+//     console.log(value)
+// }
+
+console.log(myarray[4])
+
+let myobject = {
+    "username":"Jon",
+    "quest":"grail",
+    "color":"Red"
+}
+
+console.log(
+    myobject.quest
+)
